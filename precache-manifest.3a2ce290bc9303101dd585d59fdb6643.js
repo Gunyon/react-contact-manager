b30b28c8c778d2894cc8bcb68dbc5373a8d0271c
Oleg Gabureac <gabureac.oleg@gmail.com>
2019-01-26 18:47:00 +0200

    Updates

---
+++ precache-manifest.3a2ce290bc9303101dd585d59fdb6643.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "af4098a5559f0e46cb80",
+    "url": "/reac-contact-manager/static/js/runtime~main.af4098a5.js"
+  },
+  {
+    "revision": "ce0410afcd6995be8e06",
+    "url": "/reac-contact-manager/static/js/main.ce0410af.chunk.js"
+  },
+  {
+    "revision": "a1a96dbbe8cf0661bd40",
+    "url": "/reac-contact-manager/static/js/1.a1a96dbb.chunk.js"
+  },
+  {
+    "revision": "ce0410afcd6995be8e06",
+    "url": "/reac-contact-manager/static/css/main.5adef023.chunk.css"
+  },
+  {
+    "revision": "a1a96dbbe8cf0661bd40",
+    "url": "/reac-contact-manager/static/css/1.a5e95b7e.chunk.css"
+  },
+  {
+    "revision": "0447e2ea799a6839774656ea0cd6aeeb",
+    "url": "/reac-contact-manager/index.html"
+  }
+];
